3673f447bfedf0ba402b9d6c3a9b7fbd10cab259
SAC-CS112-Christin-Walder <cdubs808@gmail.com>
2015-09-28 20:24:48 -0700

    Second Commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,26 +6,26 @@ public class Calculator
 	{
 		Scanner input = new Scanner(System.in);
 		
-		int number1;
-		int number2;
-		int sum;
-		int difference;
-		int product;
-		int quotient;
-		int calculator;
+		double number1;
+		double number2;
+		double sum;
+		double difference;
+		double product;
+		double quotient;
+		double calculator;
 		
 		
 		
-		System.out.print("Enter first integer: ");
-		number1=input.nextInt();
+		System.out.print("Enter first number: ");
+		number1=input.nextDouble();
 		
-		System.out.print("Enter second integer: ");
-		number2=input.nextInt();
+		System.out.print("Enter second number: ");
+		number2=input.nextDouble();
 		
 		System.out.println("What would you like to calculate?");
 		System.out.println("Enter 1 for sum \nEnter 2 for difference \n"
 				+ "Enter 3 for product \nEnter 4 for quotient");
-		calculator=input.nextInt();
+		calculator=input.nextDouble();
 		
 		sum = number1 + number2;
 		difference = number1 - number2;
@@ -33,13 +33,13 @@ public class Calculator
 		quotient = number1 / number2;
 		
 		if (calculator==1)
-			System.out.printf("Sum: %d\n", sum);
+			System.out.printf("Sum: %.2f\n", sum);
 		else if (calculator==2)
-			System.out.printf("Difference: %d\n", difference);
+			System.out.printf("Difference: %.2f\n", difference);
 		else if (calculator==3)
-			System.out.printf("Product: %d\n", product);
+			System.out.printf("Product: %.2f\n", product);
 		else if (calculator==4)
-			System.out.printf("Quotient: %d\n", quotient);
+			System.out.printf("Quotient: %.2f\n", quotient);
 		else
 			System.out.print("You made an invalid selection");
 	

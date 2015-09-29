b79e3b940768c07eff26498dbe55d8532b70278a
SAC-CS112-Christin-Walder <cdubs808@gmail.com>
2015-09-28 23:03:00 -0700

    Third Commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,43 +6,26 @@ public class Calculator
 	{
 		Scanner input = new Scanner(System.in);
 		
-		double number1;
-		double number2;
-		double sum;
-		double difference;
-		double product;
-		double quotient;
-		double calculator;
+		System.out.print("Enter first integer: ");
+		double n1=input.nextDouble();
 		
+		System.out.print("Enter second integer: ");
+		double n2=input.nextDouble();
 		
+		System.out.print("Select one: +, -, *, or /: ");
+		String operator=input.next();
 		
-		System.out.print("Enter first number: ");
-		number1=input.nextDouble();
-		
-		System.out.print("Enter second number: ");
-		number2=input.nextDouble();
-		
-		System.out.println("What would you like to calculate?");
-		System.out.println("Enter 1 for sum \nEnter 2 for difference \n"
-				+ "Enter 3 for product \nEnter 4 for quotient");
-		calculator=input.nextDouble();
-		
-		sum = number1 + number2;
-		difference = number1 - number2;
-		product = number1 * number2;
-		quotient = number1 / number2;
-		
-		if (calculator==1)
-			System.out.printf("Sum: %.2f\n", sum);
-		else if (calculator==2)
-			System.out.printf("Difference: %.2f\n", difference);
-		else if (calculator==3)
-			System.out.printf("Product: %.2f\n", product);
-		else if (calculator==4)
-			System.out.printf("Quotient: %.2f\n", quotient);
+		if (operator.equals("+"))
+			System.out.printf("\nCalculation: %.0f + %.0f = %.0f", n1, n2, n1 + n2);
+		else if (operator.equals("-"))
+			System.out.printf("\nCalculation: %.0f - %.0f = %.0f", n1, n2, n1 - n2);
+		else if (operator.equals("*"))
+			System.out.printf("\nCalculation: %.0f * %.0f = %.0f", n1, n2, n1 * n2);
+		else if (operator.equals("/"))
+			System.out.printf("\nCalculation: %.0f / %.0f = %.1f", n1, n2, n1 / n2);
 		else
-			System.out.print("You made an invalid selection");
-	
+			System.out.print("You have made an invalid selection.");
+
 	}
 
 }
